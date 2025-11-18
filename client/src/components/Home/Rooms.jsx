b5185dbfd05ece9846e8f8e0a2cb35a8699be1ea
tnayem <tasdidnayem@gmail.com>
2025-11-18 08:25:13 +0600

    fetch room data with transtack query

--- client/src/components/Home/Rooms.jsx
+++ client/src/components/Home/Rooms.jsx
@@ -1,30 +1,27 @@
-import { useEffect, useState } from 'react'
 import Card from './Card'
 import Container from '../Shared/Container'
 import Heading from '../Shared/Heading'
 import LoadingSpinner from '../Shared/LoadingSpinner'
+import { useQuery } from '@tanstack/react-query'
+import useAxiosSecure from '../../hooks/useAxiosSecure'
 
 const Rooms = () => {
-  const [rooms, setRooms] = useState([])
-  const [loading, setLoading] = useState(false)
+  const axiosSecure = useAxiosSecure()
+  const {data,isLoading} = useQuery({
+    queryKey:["rooms"],
+    queryFn:async()=>{
+      const {data} = await axiosSecure.get('/rooms')
+      return data
+    }
+    })
 
-  useEffect(() => {
-    setLoading(true)
-    fetch(`http://localhost:8000/rooms`)
-      .then(res => res.json())
-      .then(data => {
-        setRooms(data)
-        setLoading(false)
-      })
-  }, [])
-
-  if (loading) return <LoadingSpinner />
+  if (isLoading) return <LoadingSpinner />
 
   return (
     <Container>
-      {rooms && rooms.length > 0 ? (
+      {data && data.length > 0 ? (
         <div className='pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
-          {rooms.map(room => (
+          {data?.map(room => (
             <Card key={room._id} room={room} />
           ))}
         </div>
--- client/src/hooks/useAxiosSecure.jsx
+++ client/src/hooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import useAuth from './useAuth'
 import { useNavigate } from 'react-router-dom'
 
 export const axiosSecure = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: `http://localhost:8000`,
   withCredentials: true,
 })
 const useAxiosSecure = () => {
